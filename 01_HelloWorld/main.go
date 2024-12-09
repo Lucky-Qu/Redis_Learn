7e50da0dfb12b755ead3b36205409c0c6182d9f8
LuckyQu <super.lucky.qu@gmail.com>
2024-12-09 10:19:28 +0800

    HelloWorld!

--- 01_HelloWorld/main.go
+++ 01_HelloWorld/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	if err := rdb.SetEx(context.Background(), "sentence", "HelloWorld", 5*time.Second).Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println(rdb.Get(context.Background(), "sentence").String())
+	time.Sleep(6 * time.Second)
+	fmt.Println(rdb.Get(context.Background(), "sentence").String())
 }
